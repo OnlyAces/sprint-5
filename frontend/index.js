a95dcdbd1f9f923b51f33f9917cf980df1270cff
Isaac Henry <isaachenry751@gmail.com>
2023-11-13 00:14:57 -0500

    commit

--- frontend/index.js
+++ frontend/index.js
@@ -13,34 +13,34 @@ if (learnersContainer) {
     learnersContainer.appendChild(learnerElement);
   });
 }
-  // 1. Fetch data from the API
-  const response1 = await axios.get('http://localhost:3003/api/learners');
-  const learners = response1.data;
+ // 1. Fetch data from the API
+const response1 = await axios.get('http://localhost:3003/api/learners');
+const learners = response1.data;
 
-  const response2 = await axios.get('http://localhost:3003/api/mentors');
-  const mentors = response2.data;
+const response2 = await axios.get('http://localhost:3003/api/mentors');
+const mentors = response2.data;
 
-  // 2. Combine data obtained from different sources into a single data structure
-  const combinedData = learners.map(learner => {
-    return {
-      ...learner,
-      mentorDetails: learner.mentors.map(mentorId =>
-        mentors.find(mentor => mentor.id === mentorId))
-    };
+// 2. Combine data obtained from different sources into a single data structure
+const combinedData = learners.map(learner => {
+  return {
+    ...learner,
+    mentorDetails: learner.mentors.map(mentorId =>
+      mentors.find(mentor => mentor.id === mentorId))
+  };
+});
+
+// 3. Render repeatable components to the DOM using the combined data
+const learnerContainer = document.querySelector('.learners-container');
+combinedData.forEach(learner => {
+  const learnerElement = document.createElement('div');
+  learnerElement.textContent = `${learner.fullName} (${learner.email})`;
+
+  learnerElement.addEventListener('click', () => {
+    // Do something when a learner is clicked
   });
 
-  // 3. Render repeatable components to the DOM using the combined data
-  const learnersContainer = document.querySelector('.learners-container');
-  combinedData.forEach(learner => {
-    const learnerElement = document.createElement('div');
-    learnerElement.textContent = `${learner.fullName} (${learner.email})`;
-
-    learnerElement.addEventListener('click', () => {
-      // Do something when a learner is clicked
-    });
-
-    learnersContainer.appendChild(learnerElement);
-  });
+  learnerContainer.appendChild(learnerElement);
+});
 }
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
