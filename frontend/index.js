ebcfaa661b393634e27ecbc369bb531767f5a565
Isaac Henry <isaachenry751@gmail.com>
2023-11-13 00:07:58 -0500

    commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,36 @@
 async function sprintChallenge5() {
-  
- 
+  // This function is used to fetch data from the API and render it to the DOM.
 
-  
+  // 1. Fetch data from the API
+  const response1 = await axios.get('http://localhost:3003/api/learners');
+  const learners = response1.data;
+
+  const response2 = await axios.get('http://localhost:3003/api/mentors');
+  const mentors = response2.data;
+
+  // 2. Combine data obtained from different sources into a single data structure
   const combinedData = learners.map(learner => {
     return {
       ...learner,
-      mentorDetails: learner.mentors.map(mentorId => 
+      mentorDetails: learner.mentors.map(mentorId =>
         mentors.find(mentor => mentor.id === mentorId))
     };
   });
 
-  
-  const learnersContainer = document.querySelector('.learners-container'); 
+  // 3. Render repeatable components to the DOM using the combined data
+  const learnersContainer = document.querySelector('.learners-container');
   combinedData.forEach(learner => {
     const learnerElement = document.createElement('div');
     learnerElement.textContent = `${learner.fullName} (${learner.email})`;
-    
-  
-    learnerElement.addEventListener('click', () => {
-    
-    });
 
+    learnerElement.addEventListener('click', () => {
+      // Do something when a learner is clicked
+    });
 
     learnersContainer.appendChild(learnerElement);
   });
 }
 
-
-sprintChallenge5();
+// ❗ DO NOT CHANGE THE CODE  BELOW
+if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
+else sprintChallenge5()
